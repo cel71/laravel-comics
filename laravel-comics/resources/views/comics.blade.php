18fa7bc0475ab690c341b82b865ffaddfc3e36bf
cel71 <celestialc71@libero.it>
2021-09-02 15:10:57 +0200

    laravel-comics es3

---
+++ laravel-comics/resources/views/comics.blade.php
@@ -0,0 +1,6 @@
+@extends('templates.base')
+@section('title', 'Comics')
+
+@section('box centrale')
+    LISTA FUMETTI
+@endsection
---
+++ laravel-comics/resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('templates.base')
+@section('title', 'HomePage')
+
+@section('box centrale')
+    HOMEPAGE
+@endsection
